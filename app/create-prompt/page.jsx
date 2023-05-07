a3099bc867c0c1c5581296ea394c88862d7e0c45
felixNyalenda <felixasejunior@gmail.com>
2023-05-07 18:31:29 +0300

    prompt api build

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -13,7 +13,31 @@ const CreatePrompt = () => {
     tag: "",
   });
 
-  const createPrompt = async (e) => {};
+  const router = useRouter();
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+          userId: session?.user.id,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
